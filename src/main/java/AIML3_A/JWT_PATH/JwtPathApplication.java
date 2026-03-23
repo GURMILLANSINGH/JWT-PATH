3b0c50b1c228c61d6e1fbc84236b810e6e59d277
GURMILLANSINGH <wandybains@gmail.com>
2026-03-23 11:42:27 +0530

    first commit

---
+++ src/main/java/AIML3_A/JWT_PATH/JwtPathApplication.java
@@ -0,0 +1,13 @@
+package AIML3_A.JWT_PATH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtPathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AIML3_A/JWT_PATH/JwtPathApplicationTests.java
@@ -0,0 +1,13 @@
+package AIML3_A.JWT_PATH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtPathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
